f9db873d748071d7f09394c815ff4cd74580f374
AskaV <caya2321@gmail.com>
2021-09-06 13:04:46 +0300

    dell folder

--- src/main/java/com/solvd/laba/dao/interfaces/generics/IEntityDAO.java
+++ src/main/java/com/solvd/laba/dao/interfaces/generics/IEntityDAO.java
@@ -1,6 +1,6 @@
 package com.solvd.laba.dao.interfaces.generics;
 
-public interface IEntityDAO <T> {
+public interface IEntityDAO <T> {// typical database queries
     public void save (T t);
     public void delete (T t);
 
