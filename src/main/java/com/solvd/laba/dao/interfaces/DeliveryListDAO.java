7566829b6d515d7dd21189799b98b90c295c8380
Katerina Tebenkova <68323049+Katerina1405@users.noreply.github.com>
2021-10-08 09:55:02 +0300

    Delete src/main directory

--- src/main/java/com/solvd/laba/dao/interfaces/DeliveryListDAO.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.laba.dao.interfaces;
-
-import com.solvd.laba.dao.jdbc.IEntityDAO;
-
-public interface DeliveryListDAO extends IEntityDAO<DeliveryListDAO> {
-}
--- src/main/java/com/solvd/laba/dao/interfaces/ProductTypeDAO.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.laba.dao.interfaces;
-
-import com.solvd.laba.dao.jdbc.IEntityDAO;
-
-public interface ProductTypeDAO extends IEntityDAO<ProductTypeDAO> {
-}
