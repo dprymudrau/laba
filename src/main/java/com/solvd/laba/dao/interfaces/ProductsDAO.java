79f8648f61ec5ba51544083a4140d1ef58a2be11
Katerina Tebenkova <68323049+Katerina1405@users.noreply.github.com>
2021-10-11 19:42:52 +0300

    Delete src/main directory

--- src/main/java/com/solvd/laba/dao/interfaces/ProductsDAO.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.laba.dao.interfaces;
-
-import com.solvd.laba.dao.jdbc.IEntityDAO;
-
-public interface ProductsDAO extends IEntityDAO<ProductsDAO> {
-}
--- src/main/java/com/solvd/laba/dao/service/impl/MyService.java
+++
@@ -1,9 +0,0 @@
-package com.solvd.laba.dao.service.impl;
-
-public interface MyService<T, U> {
-    void doSmth();
-    T getById(U id);
-    T setById(U id);
-    T checkDb(U id);
-}
-
